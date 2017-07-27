548eea3f4a2a90e45ce96bc57fa2ec0166020bbd
Tomek <prysak.tomasz@gmail.com>
2017-07-27 09:15:40 +0200

    Dzien_4 programownaia w pythonie

--- dzien_3/python-dzien_3_funkcje.py
+++ dzien_3/python-dzien_3_funkcje.py
@@ -157,3 +157,26 @@ def odleglosc(x1,y1,x2,y2):
     return round(d, 2)
 
 print(odleglosc(1,1,5,5))
+
+# ćwiczenie 45
+
+def odleglosc(x1,y1,x2,y2):
+    d = sqrt(((x2-x1)**2)+((y2-y1)**2))
+    return round(d, 2)
+
+i = 0
+x = []
+y = []
+
+while (i < 2):
+    if (i == 1):
+        print("Podaj położenie końcowe:")
+    else:
+        print("Podaj położenie początkowe:")
+    u1 = int(input(" "))
+    u2 = int(input(" "))
+    x.append(u1)
+    y.append(u2)
+    i = i + 1
+
+print("Przejdziesz dystans: ", odleglosc(x[0],y[0],x[1],y[1]))
